8942638f88a17ed9f72b0ceea78c1784fd8e0133
aden-q <qian0102@umn.edu>
2022-05-30 12:00:49 -0500

    Complete project 3 - insert

--- src/execution/insert_executor.cpp
+++ src/execution/insert_executor.cpp
@@ -39,21 +39,36 @@ bool InsertExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) {
   TableHeap *table_ = table_info_->table_.get();
   BUSTUB_ASSERT(table_info_ != nullptr, "The table info is nullptr.");
   const Schema &schema = table_info_->schema_;
+  Tuple tuple_temp;
+  RID rid_temp;
+  std::vector<Tuple> tuples;
   // Query the plan to check the type of insert
   if (plan_->IsRawInsert()) {
     // Raw insert
     // It is possible that there are multiple records to be inserted
-    // Insert into the table directly and update the indexes
+    // Read the tuples to be inserted
     for (auto &vals : plan_->RawValues()) {
-      table_->InsertTuple(Tuple(vals, &schema), rid, exec_ctx_->GetTransaction());
+      tuple_temp = Tuple(vals, &schema);
+      tuples.push_back(tuple_temp);
     }
   } else {
     // Insert from a sub-query
     // First execute the child plan
     // Get the results from the child executor
-    // Insert the results into the current plan and update the indexes
+    while (child_executor_->Next(&tuple_temp, &rid_temp)) {
+      tuples.push_back(tuple_temp);
+    }
   }
-
+  // Insert into the table
+  for (auto &next_tuple : tuples) {
+    table_->InsertTuple(next_tuple, rid, exec_ctx_->GetTransaction());
+    // Update indexes for each inserted row
+    for (auto index_info : index_info_vec_) {
+      index_info->index_->InsertEntry(
+          next_tuple.KeyFromTuple(schema, *index_info->index_->GetKeySchema(), index_info->index_->GetKeyAttrs()),
+          *rid, exec_ctx_->GetTransaction());
+    }
+  }
   return false;
 }
 
--- test/execution/executor_test.cpp
+++ test/execution/executor_test.cpp
@@ -111,7 +111,7 @@ TEST_F(ExecutorTest, SimpleSeqScanTest) {
 }
 
 // INSERT INTO empty_table2 VALUES (100, 10), (101, 11), (102, 12)
-TEST_F(ExecutorTest, DISABLED_SimpleRawInsertTest) {
+TEST_F(ExecutorTest, SimpleRawInsertTest) {
   // Create Values to insert
   std::vector<Value> val1{ValueFactory::GetIntegerValue(100), ValueFactory::GetIntegerValue(10)};
   std::vector<Value> val2{ValueFactory::GetIntegerValue(101), ValueFactory::GetIntegerValue(11)};
@@ -153,7 +153,7 @@ TEST_F(ExecutorTest, DISABLED_SimpleRawInsertTest) {
 }
 
 // INSERT INTO empty_table2 SELECT col_a, col_b FROM test_1 WHERE col_a < 500
-TEST_F(ExecutorTest, DISABLED_SimpleSelectInsertTest) {
+TEST_F(ExecutorTest, SimpleSelectInsertTest) {
   const Schema *out_schema1;
   std::unique_ptr<AbstractPlanNode> scan_plan1;
   {
@@ -205,7 +205,7 @@ TEST_F(ExecutorTest, DISABLED_SimpleSelectInsertTest) {
 }
 
 // INSERT INTO empty_table2 VALUES (100, 10), (101, 11), (102, 12)
-TEST_F(ExecutorTest, DISABLED_SimpleRawInsertWithIndexTest) {
+TEST_F(ExecutorTest, SimpleRawInsertWithIndexTest) {
   // Create Values to insert
   std::vector<Value> val1{ValueFactory::GetIntegerValue(100), ValueFactory::GetIntegerValue(10)};
   std::vector<Value> val2{ValueFactory::GetIntegerValue(101), ValueFactory::GetIntegerValue(11)};
